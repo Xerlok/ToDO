5ee09a9c8be0b9d2641b19bc2bb8586422ef7df8
Xerlok <20791727+Xerlok@users.noreply.github.com>
2023-12-22 00:41:12 +0300

    Plase showModal function in a separate module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable max-len */
 import './styles.css';
-import 'webcimes-modal/dist/css/webcimes-modal.css';
-import { WebcimesModal } from 'webcimes-modal';
+import showModal from './showModal';
 import cacheDOM from './dom';
 
 (() => {
@@ -202,7 +201,7 @@ import cacheDOM from './dom';
       });
       newProject.slideMenuBtnDel.addEventListener('click', (e) => {
         e.stopPropagation();
-        toDO.showModal('project', newProject.project);
+        showModal('project', newProject.project, toDO);
       });
       newProject.slideMenuBtnEdit.addEventListener('click', (e) => {
         e.stopPropagation();
@@ -242,7 +241,7 @@ import cacheDOM from './dom';
         toDO.dropElement(e);
       });
       newTodo.slideMenuBtnDel.addEventListener('click', () => {
-        toDO.showModal('todo', newTodo.todo);
+        showModal('todo', newTodo.todo, toDO);
       });
       newTodo.slideMenuBtnEdit.addEventListener('click', () => {
         toDO.makeElementEditable('todo', newTodo);
@@ -262,39 +261,6 @@ import cacheDOM from './dom';
       if (localStorage.getItem('projects') != null) {
         toDO.projects = JSON.parse(localStorage.getItem('projects'));
       }
-    },
-    showModal: function showModal(type, obj) {
-      // eslint-disable-next-line no-unused-vars
-      const myModal = new WebcimesModal({
-        setId: null, // set a specific id on the modal. default "null"
-        setClass: null, // set a specific class on the modal, default "null"
-        width: 'auto', // width (specify unit), default "auto"
-        height: 'auto', // height (specify unit), default "auto"
-        titleHtml: "<span style='color:red'>Delete?</span>", // html for title, default "null"
-        bodyHtml: null, // html for body, default "null"
-        buttonCancelHtml: 'Cancel', // html for cancel button, default "null"
-        buttonConfirmHtml: 'Confirm', // html for confirm button, default "null"
-        closeOnCancelButton: true, // close modal after trigger cancel button, default "true"
-        closeOnConfirmButton: true, // close modal after trigger confirm button, default "true"
-        showCloseButton: true, // show close button, default "true"
-        allowCloseOutside: true, // allow the modal to close when clicked outside, default "true"
-        allowMovement: true, // ability to move modal, default "true"
-        moveFromHeader: true, // if allowMovement is set to "true", ability to move modal from header, default "true"
-        moveFromBody: false, // if allowMovement is set to "true", ability to move modal from body, default "false"
-        moveFromFooter: true, // if allowMovement is set to "true", ability to move modal from footer, default "true"
-        stickyHeader: true, // keep header sticky (visible) when scrolling, default "true"
-        stickyFooter: true, // keep footer sticky (visible) when scrolling, default "true"
-        style: 'background:#fd8d8d;', // add extra css style to modal, default null
-        animationOnShow: 'animDropDown', // "animDropDown" or "animFadeIn" for show animation, default "animDropDown"
-        animationOnDestroy: 'animDropUp', // "animDropUp" or "animFadeOut" for destroy animation, default "animDropUp"
-        animationDuration: 500, // animation duration in ms, default "500"
-        beforeShow: () => {}, // callback before show modal
-        afterShow: () => {}, // callback after show modal
-        beforeDestroy: () => {}, // callback before destroy modal
-        afterDestroy: () => {}, // callback after destroy modal
-        onCancelButton: () => {}, // callback after triggering cancel button
-        onConfirmButton: () => { toDO.deleteElement(type, obj); }, // callback after triggering confirm button
-      });
     },
   };
 
